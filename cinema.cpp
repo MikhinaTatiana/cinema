c12272480c65bbaa096846969451768bf5f80ecc
MikhinaTatiana <mikhinato@yandex.ru>
2026-05-13 23:39:28 +0300

    Create cinema.cpp

---
+++ cinema.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int maxDistToClosest(vector<int>& seats) {
+    int last = -1;
+    int ans = 0;
+
+    for (int i = 0; i < seats.size(); i++) {
+        if (seats[i] == 1) {
+
+            // если это первое занятое место
+            if (last == -1)
+                ans = i;
+            else
+                ans = max(ans, (i - last) / 2);
+
+            last = i;
+        }
+    }
+
+    // свободные места в конце
+    ans = max(ans, (int)seats.size() - 1 - last);
+
+    return ans;
+}
+
+int main() {
+    vector<int> seats = {1, 0, 0, 0, 1, 0, 1};
+
+    cout << maxDistToClosest(seats);
+
+    return 0;
+}
